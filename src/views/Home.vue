ecf4373674a38bf51c826574559c108271d98af7
Kaylee Cummings <kayleercummings@gmail.com>
2020-08-21 19:08:51 -0400

    WIP still figuring out how to customize Cognito within Vue Amplify app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <DegreeSee msg="Welcome to DegreeSee" />
     <img alt="Vue logo" src="../assets/dashboard.png" />
+    <!-- <Register /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import DegreeSee from "@/components/DegreeSee.vue";
+// import Register from "@/components/Register.vue";
 
 export default {
   name: "Home",
   components: {
     DegreeSee
+    // Register
   }
 };
 </script>
---
+++ src/views/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <amplify-authenticator
+      v-if="authState !== 'signedin'"
+    ></amplify-authenticator>
+    <div v-if="authState === 'signedin' && user">
+      <amplify-sign-out></amplify-sign-out>
+      <div>Hello, {{ user.email }}</div>
+    </div>
+  </div>
+</template>
