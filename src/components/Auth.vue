3bfbc93dd525905efc68afe40813d1bdc5a1079c
Kaylee Cummings <kayleercummings@gmail.com>
2020-09-08 15:14:47 -0500

    added custom auth form code

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,18 +1,27 @@
 <template>
 	<div>
-		<amplify-authenticator v-if="authState !== 'signedin'"/>
+		<amplify-authenticator username-alias="email" v-if="authState !== 'signedin'">
+			<amplify-sign-up
+			slot="sign-up"
+			username-alias="email"
+			:form-fields.prop="formFields"
+		></amplify-sign-up>
+		</amplify-authenticator>
+
+		<!-- <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in> -->
 		<div v-if="authState === 'signedin' && user">
-			<amplify-sign-out />
-			<div >Hello, {{ user.attributes.email }}</div>
+			<amplify-sign-out></amplify-sign-out>
+			<div>Hello, {{ user.attributes.email }}</div>
+			<iframe src="https://iot.app.initialstate.com/embed/#/tiles/bkt_1g4458k8tkfd1i"></iframe>
 		</div>
 	</div>
 </template>
 
 <script>
-import { onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { onAuthUIStateChange } from '@aws-amplify/ui-components';
 
 export default {
-	name: "AuthStateApp",
+	name: 'AuthStateApp', // or AuthWithSlots
 	created() {
 		onAuthUIStateChange((authState, authData) => {
 			this.authState = authState;
@@ -22,7 +31,28 @@ export default {
 	data() {
 		return {
 			user: undefined,
-			authState: undefined
+			authState: undefined,
+			formFields: [
+				{
+					type: 'email',
+					label: 'Email Address',
+					placeholder: 'Enter your email address',
+					required: true,
+				},
+				{
+					type: 'password',
+					label: 'Password',
+					placeholder: 'Enter your password',
+					required: true,
+				},
+				{
+					type: 'custom:desktop',
+					label: 'Invitation Code',
+					placeholder: 'Enter your invitation code',
+					required: true,
+				},
+
+			]
 		};
 	},
 	beforeDestroy() {
@@ -30,3 +60,29 @@ export default {
 	}
 };
 </script>
+
+<style scoped>
+amplify-authenticator {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex: 1;
+	
+	/* height: 100vh; */
+}
+
+amplify-button.button {
+	background-color: #3fc5ea;
+	color: #3fc5ea;
+}
+
+iframe {
+	position: relative;
+	list-style:none;
+	padding: 12px 0 0 0;
+	margin: 0;
+	perspective: 1000;
+	flex: 1 1 auto;
+	overflow: auto;
+}
+</style>
